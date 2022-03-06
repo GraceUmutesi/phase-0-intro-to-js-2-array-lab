f164527e3d11d14b1eca68731f88bc5688b6281f
GraceUmutesi <tesigrace98@gmail.com>
2022-03-06 13:40:37 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+
+var cats=['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+destructivelyAppendCat('Ralph');
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+
+destructivelyPrependCat('Bob');
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+destructivelyRemoveFirstCat();
+
+function appendCat(name){
+    var newArray=[...cats,name];
+    console.log(newArray);
+    return newArray;
+}
+appendCat("Broom");
+
+function prependCat(name){
+    var newArray=[name,...cats];
+    console.log(newArray);
+    return newArray;
+}
+appendCat("Arnold");
+
+function removeLastCat(){
+    var newArray=[...cats];
+    newArray.pop();
+    return newArray;
+
+}
+removeLastCat();
+function removeFirstCat(){
+    var newArray=[...cats];
+    newArray.shift();
+    return newArray;
+
+}
+removeFirstCat();
